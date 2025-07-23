5e08625c95a5e6838b7077d7d02ae25e3dea8cf9
wxn0brP <toja.filiper@gmail.com>
2025-07-23 18:00:29 +0200

    feat: add generic types to CollectionManager

--- src/helpers/CollectionManager.ts
+++ src/helpers/CollectionManager.ts
@@ -10,56 +10,56 @@ class CollectionManager {
     /**
      * Add data to a database.
      */
-    async add<T = Data>(data: Arg, id_gen: boolean = true) {
+    async add<T = Data>(data: Arg<T>, id_gen: boolean = true) {
         return await this.db.add(this.collection, data, id_gen) as T;
     }
 
     /**
      * Find data in a database.
      */
-    async find<T = Data>(search: Search, context: VContext = {}, options: DbFindOpts = {}, findOpts: FindOpts = {}) {
+    async find<T = Data>(search: Search<T>, context: VContext = {}, options: DbFindOpts<T> = {}, findOpts: FindOpts<T> = {}) {
         return await this.db.find(this.collection, search, context, options, findOpts) as T[];
     }
 
     /**
      * Find one data entry in a database.
      */
-    async findOne<T = Data>(search: Search, context: VContext = {}, findOpts: FindOpts = {}) {
+    async findOne<T = Data>(search: Search<T>, context: VContext = {}, findOpts: FindOpts<T> = {}) {
         return await this.db.findOne(this.collection, search, context, findOpts) as (T | null);
     }
 
     /**
      * Update data in a database.
      */
-    async update(search: Search, updater: Updater, context: VContext = {}) {
+    async update<T = Data>(search: Search<T>, updater: Updater<T>, context: VContext = {}) {
         return await this.db.update(this.collection, search, updater, context) as boolean;
     }
 
     /**
      * Update one data entry in a database.
      */
-    async updateOne(search: Search, updater: Updater, context: VContext = {}) {
+    async updateOne<T = Data>(search: Search<T>, updater: Updater<T>, context: VContext = {}) {
         return await this.db.updateOne(this.collection, search, updater, context) as boolean;
     }
 
     /**
      * Remove data from a database.
      */
-    async remove(search: Search, context: VContext = {}) {
+    async remove<T = Data>(search: Search<T>, context: VContext = {}) {
         return await this.db.remove(this.collection, search, context) as boolean;
     }
 
     /**
      * Remove one data entry from a database.
      */
-    async removeOne(search: Search, context: VContext = {}) {
+    async removeOne<T = Data>(search: Search<T>, context: VContext = {}) {
         return await this.db.removeOne(this.collection, search, context) as boolean;
     }
 
     /**
      * Asynchronously updates one entry in a database or adds a new one if it doesn't exist.
      */
-    async updateOneOrAdd(search: Search, updater: Updater, add_arg: Arg = {}, context: VContext = {}, id_gen: boolean = true) {
+    async updateOneOrAdd<T = Data>(search: Search<T>, updater: Updater<T>, add_arg: Arg<T> = {}, context: VContext = {}, id_gen: boolean = true) {
         return await this.db.updateOneOrAdd(this.collection, search, updater, add_arg, context, id_gen) as boolean;
     }
 }

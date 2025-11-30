247c37131597509daa5d2128a600ceca13cc7097
wxn0brP <toja.filiper@gmail.com>
2025-11-30 13:28:07 +0100

    fix: CollectionManager types

--- src/helpers/CollectionManager.ts
+++ src/helpers/CollectionManager.ts
@@ -17,42 +17,42 @@ class CollectionManager<D = Data> {
     /**
      * Find data in a database.
      */
-    async find<T = Data>(search: Search<T & D> = {}, options: DbFindOpts<T & Data> = {}, findOpts: FindOpts<T & Data> = {}, context: VContext = {}) {
+    async find<T = Data>(search: Search<T & D> = {}, options: DbFindOpts<T & D> = {}, findOpts: FindOpts<T & D> = {}, context: VContext = {}) {
         return await this.db.find(this.collection, search, options, findOpts, context) as T[];
     }
 
     /**
      * Find one data entry in a database.
      */
-    async findOne<T = Data>(search: Search<T & Data> = {}, findOpts: FindOpts<T & Data> = {}, context: VContext = {}) {
+    async findOne<T = Data>(search: Search<T & D> = {}, findOpts: FindOpts<T & D> = {}, context: VContext = {}) {
         return await this.db.findOne(this.collection, search, findOpts, context) as (T | null);
     }
 
     /**
      * Update data in a database.
      */
-    async update<T = Data>(search: Search<T & Data>, updater: Updater<T & Data>, context: VContext = {}) {
+    async update<T = Data>(search: Search<T & D>, updater: Updater<T & D>, context: VContext = {}) {
         return await this.db.update(this.collection, search, updater, context) as boolean;
     }
 
     /**
      * Update one data entry in a database.
      */
-    async updateOne<T = Data>(search: Search<T & Data>, updater: Updater<T & Data>, context: VContext = {}) {
+    async updateOne<T = Data>(search: Search<T & D>, updater: Updater<T & D>, context: VContext = {}) {
         return await this.db.updateOne(this.collection, search, updater, context) as boolean;
     }
 
     /**
      * Remove data from a database.
      */
-    async remove<T = Data>(search: Search<T & Data>, context: VContext = {}) {
+    async remove<T = Data>(search: Search<T & D>, context: VContext = {}) {
         return await this.db.remove(this.collection, search, context) as boolean;
     }
 
     /**
      * Remove one data entry from a database.
      */
-    async removeOne<T = Data>(search: Search<T & Data>, context: VContext = {}) {
+    async removeOne<T = Data>(search: Search<T & D>, context: VContext = {}) {
         return await this.db.removeOne(this.collection, search, context) as boolean;
     }
 
@@ -60,13 +60,13 @@ class CollectionManager<D = Data> {
      * Asynchronously updates one entry in a database or adds a new one if it doesn't exist.
      */
     async updateOneOrAdd<T = Data>(
-        search: Search<T & Data>,
-        updater: Updater<T & Data>,
+        search: Search<T & D>,
+        updater: Updater<T & D>,
         {
             add_arg = {},
             context = {},
             id_gen = true
-        }: UpdateOneOrAdd<T & Data>
+        }: UpdateOneOrAdd<T & D>
     ) {
         return await this.db.updateOneOrAdd(this.collection, search, updater, { add_arg, context, id_gen }) as boolean;
     }
@@ -74,7 +74,7 @@ class CollectionManager<D = Data> {
     /**
      * Asynchronously removes one entry in a database or adds a new one if it doesn't exist. Usage e.g. for toggling a flag.
      */
-    async toggleOne<T = Data>(search: Search<T & Data>, data: Arg<T & Data> = {}, context: VContext = {}) {
+    async toggleOne<T = Data>(search: Search<T & D>, data: Arg<T & D> = {}, context: VContext = {}) {
         return await this.db.toggleOne(this.collection, search, data, context) as boolean;
     }
 }

d2c57cba5584055d2d6190a80fba974498698a16
wxn0brP <toja.filiper@gmail.com>
2025-11-30 15:22:46 +0100

    docs: update search & updater

--- src/types/searchOpts.ts
+++ src/types/searchOpts.ts
@@ -21,41 +21,61 @@ export type LogicalOperators<T = any> = {
 
 /** Comparison Operators with nested support */
 export type ComparisonOperators<T = any> = {
+	/** 5 > 4 */
 	$gt?: NestedValue<T, number, number>;
+	/** 5 < 4 */
 	$lt?: NestedValue<T, number, number>;
+	/** 5 >= 4 */
 	$gte?: NestedValue<T, number, number>;
+	/** 5 <= 4 */
 	$lte?: NestedValue<T, number, number>;
+	/** 5 between [min, max] */
 	$between?: NestedValue<T, [number, number], number>;
+	/** 2 in [1, 2, 3] */
 	$in?: DeepPartial<T> & { [K in keyof T]?: T[K] extends any[] ? T[K] : T[K][] };
+	/** 5 not in [1, 2, 3] */
 	$nin?: DeepPartial<T> & { [K in keyof T]?: T[K] extends any[] ? T[K] : T[K][] };
+	/** id > 4 */
 	$idGt?: NestedValue<T, string | number, string | number>;
+	/** id < 4 */
 	$idLt?: NestedValue<T, string | number, string | number>;
+	/** id >= 4 */
 	$idGte?: NestedValue<T, string | number, string | number>;
+	/** id <= 4 */
 	$idLte?: NestedValue<T, string | number, string | number>;
 };
 
 /** Type and Existence Operators with nested support */
 export type TypeAndExistenceOperators<T = any> = {
+	/** "name" in { name: "John" } */
 	$exists?: NestedValue<T, boolean>;
+	/** "name" == "string" in { name: "John" } */
 	$type?: NestedValue<T, string>;
 };
 
 /** Array Operators with nested support */
 export type ArrayOperators<T = any> = {
+	/** [1, 2, 3] includes 2 */
 	$arrinc?: DeepPartial<T>;
+	/** [1, 2, 3] array includes all elements e.g. [1, 2] */
 	$arrincall?: DeepPartial<T>;
+	/** [1, 2, 3] has size 3 */
 	$size?: NestedValue<T, number>;
 };
 
 /** String Operators with nested support */
 export type StringOperators<T = any> = {
+	/** "John" matches /oh/ */
 	$regex?: NestedValue<T, RegExp | string, string>;
+	/** "John" starts with "Jo" */
 	$startsWith?: NestedValue<T, string, string>;
+	/** "John" ends with "hn" */
 	$endsWith?: NestedValue<T, string, string>;
 };
 
 /** Other Operators with nested support */
 export type OtherOperators<T = any> = {
+	/** { $type: "name" } matches { $type: "name" } literally - Ignore $ operators */
 	$subset?: DeepPartial<T>;
 };
 
--- src/types/updater.ts
+++ src/types/updater.ts
@@ -1,31 +1,40 @@
-/**
- * Predefined type for updating data.
- */
-
 import { DeepPartial, NestedValue } from "./utils";
 
 /** Arrays */
 export type ArrayUpdater<T = any> = {
+    /** [1,2] -> $push 3 -> [1,2,3] */
     $push?: NestedValue<T, any>,
-    /** Pushes items into an array and removes duplicates */
+    /** [1,2] -> $pushset 2,3 -> [1,2,3] */
     $pushset?: NestedValue<T, any>,
+    /** [1,2,3] -> $pull 2 -> [1,3] */
     $pull?: NestedValue<T, any>,
+    /** [1,2,2,3] -> $pullall [2] -> [1,3] */
     $pullall?: NestedValue<T, any>,
 }
 
 /** Objects */
 export type ObjectUpdater<T = any> = {
+    /** { a: 1 } -> $merge { b: 2 } -> { a: 1, b: 2 } */
     $merge?: NestedValue<T, any>,
+    /** { a: { x: 1 } } -> $deepMerge { a: { y: 2 } } -> { a: { x: 1, y: 2 } } */
     $deepMerge?: NestedValue<T, any>,
 }
 
 /** Values */
 export type ValueUpdater<T = any> = {
+    /** { count: 1 } -> $inc 2 -> { count: 3 } */
     $inc?: NestedValue<T, number>,
+    /** { count: 5 } -> $dec 2 -> { count: 3 } */
     $dec?: NestedValue<T, number>,
+    /** { name: "John" } -> $unset "name" -> {} */
     $unset?: NestedValue<T, any>,
+    /** { oldName: "value" } -> $rename "oldName" to "newName" -> { newName: "value" } */
     $rename?: NestedValue<T, any>,
-    /** same as { name: value } */
+    /** 
+     * {} -> $set { name: "John" } -> { name: "John" } 
+     * 
+     * Note: same as { name: value } 
+     */
     $set?: NestedValue<T, any>,
 }
 

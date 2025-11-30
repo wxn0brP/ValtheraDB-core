66bf502013481fb0a78009bdfad908a7a636bdb4
wxn0brP <toja.filiper@gmail.com>
2025-11-30 17:06:35 +0100

    feat: change deepMerge

--- src/utils/merge.ts
+++ src/utils/merge.ts
@@ -1,22 +1,26 @@
-export function deepMerge(target: any, source: any): any {
-    const output = { ...target };
+export function _deepMerge(target: Record<string, any>, ...sources: Record<string, any>[]): Record<string, any> {
+    if (!sources.length) return target;
+    const source = sources.shift();
+    if (!target) target = {};
+
     if (isObject(target) && isObject(source)) {
         for (const key in source) {
-            if (Object.prototype.hasOwnProperty.call(source, key)) {
-                if (isObject(source[key])) {
-                    if (!target[key]) {
-                        output[key] = {};
-                    }
-                    output[key] = deepMerge(target[key], source[key]);
-                } else {
-                    output[key] = source[key];
-                }
+            if (isObject(source[key])) {
+                if (!target[key]) Object.assign(target, { [key]: {} });
+                _deepMerge(target[key], source[key]);
+            } else {
+                Object.assign(target, { [key]: source[key] });
             }
         }
     }
-    return output;
+
+    return _deepMerge(target, ...sources);
+}
+
+export function deepMerge(target: Record<string, any>, ...sources: Record<string, any>[]): Record<string, any> {
+    return _deepMerge(structuredClone(target), ...sources.map(s => structuredClone(s)));
 }
 
 function isObject(item: any): boolean {
-    return item !== null && typeof item === "object" && !Array.isArray(item);
+    return (item && typeof item === "object" && !Array.isArray(item));
 }

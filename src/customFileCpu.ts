b86de6ebbfc72ca3c1b2cee63c77b9ad2135be25
wxn0brP <toja.filiper@gmail.com>
2026-05-14 16:16:00 +0200

    refactor: format code

--- src/customFileCpu.ts
+++ src/customFileCpu.ts
@@ -5,8 +5,8 @@ import { hasFieldsAdvanced } from "./utils/hasFieldsAdvanced";
 import { updateFindObject } from "./utils/updateFindObject";
 import { updateObjectAdvanced } from "./utils/updateObject";
 
-export type WriteFile = (file: string, data: any[]) => Promise<void>
-export type ReadFile = (file: string) => Promise<any[]>
+export type WriteFile = (file: string, data: any[]) => Promise<void>;
+export type ReadFile = (file: string) => Promise<any[]>;
 
 export function pathRepair(path: string) {
     return path.replaceAll("//", "/");
@@ -34,40 +34,52 @@ export class CustomFileCpu implements FileCpu {
 
         const { search, context = {}, findOpts = {} } = config;
 
-        const results = entries.filter(entry =>
-            typeof search === "function" ? search(entry, context) : hasFieldsAdvanced(entry, search)
+        const results = entries.filter((entry) =>
+            typeof search === "function"
+                ? search(entry, context)
+                : hasFieldsAdvanced(entry, search),
         );
 
-        return results.length ?
-            results.map(res => updateFindObject(res, findOpts)) :
-            [];
+        return results.length
+            ? results.map((res) => updateFindObject(res, findOpts))
+            : [];
     }
 
-    async findOne(file: string, config: VQueryT.FindOne): Promise<Data | false> {
+    async findOne(
+        file: string,
+        config: VQueryT.FindOne,
+    ): Promise<Data | false> {
         file = pathRepair(file);
         const entries = await this._readFile(file);
 
         const { search, context = {}, findOpts = {} } = config;
 
-        const result = entries.find(entry =>
-            typeof search === "function" ? search(entry, context) : hasFieldsAdvanced(entry, search)
+        const result = entries.find((entry) =>
+            typeof search === "function"
+                ? search(entry, context)
+                : hasFieldsAdvanced(entry, search),
         );
-        return result ?
-            updateFindObject(result, findOpts) :
-            false;
+        return result ? updateFindObject(result, findOpts) : false;
     }
 
-    async remove(file: string, config: VQueryT.Remove, one: boolean): Promise<Data[]> {
+    async remove(
+        file: string,
+        config: VQueryT.Remove,
+        one: boolean,
+    ): Promise<Data[]> {
         file = pathRepair(file);
         let entries = await this._readFile(file);
         const removed = [];
 
         const { search, context = {} } = config;
 
-        entries = entries.filter(entry => {
+        entries = entries.filter((entry) => {
             if (removed.length && one) return true;
 
-            let match = typeof search === "function" ? search(entry, context) : hasFieldsAdvanced(entry, search);
+            let match =
+                typeof search === "function"
+                    ? search(entry, context)
+                    : hasFieldsAdvanced(entry, search);
 
             if (match) {
                 removed.push(entry);
@@ -77,26 +89,35 @@ export class CustomFileCpu implements FileCpu {
             return true;
         });
 
-        if (removed.length)
-            await this._writeFile(file, entries);
+        if (removed.length) await this._writeFile(file, entries);
 
         return removed;
     }
 
-    async update(file: string, config: VQueryT.Update, one: boolean): Promise<Data[]> {
+    async update(
+        file: string,
+        config: VQueryT.Update,
+        one: boolean,
+    ): Promise<Data[]> {
         file = pathRepair(file);
         let entries = await this._readFile(file);
         const updated = [];
 
         const { search, updater, context = {} } = config;
 
-        entries = entries.map(entry => {
+        entries = entries.map((entry) => {
             if (updated.length && one) return entry;
 
-            let match = typeof search === "function" ? search(entry, context) : hasFieldsAdvanced(entry, search);
+            let match =
+                typeof search === "function"
+                    ? search(entry, context)
+                    : hasFieldsAdvanced(entry, search);
 
             if (match) {
-                const updatedEntry = typeof updater === "function" ? updater(entry, context) : updateObjectAdvanced(entry, updater);
+                const updatedEntry =
+                    typeof updater === "function"
+                        ? updater(entry, context)
+                        : updateObjectAdvanced(entry, updater);
                 updated.push(updatedEntry);
                 return updatedEntry;
             }
@@ -104,8 +125,7 @@ export class CustomFileCpu implements FileCpu {
             return entry;
         });
 
-        if (updated.length)
-            await this._writeFile(file, entries);
+        if (updated.length) await this._writeFile(file, entries);
 
         return updated;
     }

fbf2182238c638ec16c8b2703d6ffefb3e79cae1
wxn0brP <viviennevaltheris@gmail.com>
2026-08-09 17:05:36 +0200

    fix: unit tests

--- test/utils/updateObject.test.ts
+++ test/utils/updateObject.test.ts
@@ -18,55 +18,52 @@ describe("updateObjectAdvanced", () => {
 	});
 
 	test("2. should handle single field object", () => {
-		const obj = { a: 1, b: 2 };
+		const obj: any = { a: 1, b: 2 };
 		const result = updateObjectAdvanced(obj, { c: 3 });
-		expect(result).toEqual({ a: 1, b: 2, c: 3 });
-		expect(obj).toEqual({ a: 1, b: 2 });
+		expect(result).toBeUndefined();
+		expect(obj).toEqual({ a: 1, b: 2, c: 3 });
 	});
 
 	test("3. should handle array of field objects", () => {
-		const obj = { a: 1, b: 2 };
+		const obj: any = { a: 1, b: 2 };
 		const result = updateObjectAdvanced(obj, { c: 3, d: 4 });
-		expect(result).toEqual({ a: 1, b: 2, c: 3, d: 4 });
-		expect(obj).toEqual({ a: 1, b: 2 });
+		expect(result).toBeUndefined();
+		expect(obj).toEqual({ a: 1, b: 2, c: 3, d: 4 });
 	});
 
 	test("4. should handle $push operator", () => {
 		const obj = { arr: [1, 2] };
 		const result = updateObjectAdvanced(obj, { $push: { arr: 3 } });
-		expect(result.arr).toEqual([1, 2, 3]);
-		expect(obj.arr).toEqual([1, 2]);
+		expect(result).toBeUndefined();
+		expect(obj.arr).toEqual([1, 2, 3]);
 
 		const obj2: Data = {};
 		const result2 = updateObjectAdvanced(obj2, { $push: { arr: 1 } });
-		expect(result2.arr).toEqual([1]);
-		expect(obj2).toEqual({});
+		expect(result2).toBeUndefined();
+		expect(obj2.arr).toEqual([1]);
 	});
 
 	test("5. should handle $pushSet operator", () => {
 		const obj = { arr: [1, 2] };
-		let result = updateObjectAdvanced(obj, { $pushSet: { arr: 2 } });
-		expect(result.arr).toEqual([1, 2]);
+		updateObjectAdvanced(obj, { $pushSet: { arr: 2 } });
 		expect(obj.arr).toEqual([1, 2]);
 
-		const originalResult = result;
-		result = updateObjectAdvanced(result, { $pushSet: { arr: 3 } });
-		expect(result.arr).toEqual([1, 2, 3]);
-		expect(originalResult.arr).toEqual([1, 2]);
+		updateObjectAdvanced(obj, { $pushSet: { arr: 3 } });
+		expect(obj.arr).toEqual([1, 2, 3]);
 	});
 
 	test("6. should handle $pull operator", () => {
 		const obj = { arr: [1, 2, 3, 2] };
 		const result = updateObjectAdvanced(obj, { $pull: { arr: 2 } });
-		expect(result.arr).toEqual([1, 3]);
-		expect(obj.arr).toEqual([1, 2, 3, 2]);
+		expect(result).toBeUndefined();
+		expect(obj.arr).toEqual([1, 3]);
 	});
 
 	test("7. should handle $pullAll operator", () => {
 		const obj = { arr: [1, 2, 3, 4, 5] };
 		const result = updateObjectAdvanced(obj, { $pullAll: { arr: [2, 4] } });
-		expect(result.arr).toEqual([1, 3, 5]);
-		expect(obj.arr).toEqual([1, 2, 3, 4, 5]);
+		expect(result).toBeUndefined();
+		expect(obj.arr).toEqual([1, 3, 5]);
 	});
 
 	test("8. should handle $merge operator", () => {
@@ -74,13 +71,13 @@ describe("updateObjectAdvanced", () => {
 		const result = updateObjectAdvanced(obj, {
 			$merge: { obj: { b: 3, c: 4 } },
 		});
-		expect(result.obj).toEqual({ a: 1, b: 3, c: 4 });
-		expect(obj.obj).toEqual({ a: 1, b: 2 });
+		expect(result).toBeUndefined();
+		expect(obj.obj).toEqual({ a: 1, b: 3, c: 4 });
 
 		const obj2: Data = {};
 		const result2 = updateObjectAdvanced(obj2, { $merge: { obj: { a: 1 } } });
-		expect(result2.obj).toEqual({ a: 1 });
-		expect(obj2).toEqual({});
+		expect(result2).toBeUndefined();
+		expect(obj2.obj).toEqual({ a: 1 });
 	});
 
 	test("9. should handle $deepMerge operator", () => {
@@ -88,27 +85,20 @@ describe("updateObjectAdvanced", () => {
 		const result = updateObjectAdvanced(obj, {
 			$deepMerge: { obj: { a: { d: 3 }, c: 4 } },
 		});
-		expect(result.obj).toEqual({ a: { b: 1, d: 3 }, c: 4 });
-		expect(obj.obj).toEqual({ a: { b: 1 }, c: 2 });
-
-		const obj2: Data = {};
-		const result2 = updateObjectAdvanced(obj2, {
-			$deepMerge: { obj: { a: 1 } },
-		});
-		expect(result2.obj).toEqual({ a: 1 });
-		expect(obj2).toEqual({});
+		expect(result).toBeUndefined();
+		expect(obj.obj).toEqual({ a: { b: 1, d: 3 }, c: 4 });
 	});
 
 	test("10. should handle $inc operator", () => {
 		const obj = { num: 5 };
 		const result = updateObjectAdvanced(obj, { $inc: { num: 3 } });
-		expect(result.num).toBe(8);
-		expect(obj.num).toBe(5);
+		expect(result).toBeUndefined();
+		expect(obj.num).toBe(8);
 
 		const obj2: Data = {};
 		const result2 = updateObjectAdvanced(obj2, { $inc: { num: 5 } });
-		expect(result2.num).toBe(5);
-		expect(obj2).toEqual({});
+		expect(result2).toBeUndefined();
+		expect(obj2.num).toBe(5);
 
 		expect(() => {
 			const obj3 = { str: "hello" };
@@ -119,13 +109,13 @@ describe("updateObjectAdvanced", () => {
 	test("11. should handle $dec operator", () => {
 		const obj = { num: 10 };
 		const result = updateObjectAdvanced(obj, { $dec: { num: 3 } });
-		expect(result.num).toBe(7);
-		expect(obj.num).toBe(10);
+		expect(result).toBeUndefined();
+		expect(obj.num).toBe(7);
 
 		const obj2: Data = {};
 		const result2 = updateObjectAdvanced(obj2, { $dec: { num: 5 } });
-		expect(result2.num).toBe(-5);
-		expect(obj2).toEqual({});
+		expect(result2).toBeUndefined();
+		expect(obj2.num).toBe(-5);
 
 		expect(() => {
 			const obj3 = { str: "hello" };
@@ -136,33 +126,31 @@ describe("updateObjectAdvanced", () => {
 	test("12. should handle $rename operator", () => {
 		const obj: Data = { oldKey: "value" };
 		const result = updateObjectAdvanced(obj, { $rename: { oldKey: "newKey" } });
-		expect(result).toEqual({ newKey: "value" });
-		expect(obj).toEqual({ oldKey: "value" });
+		expect(result).toBeUndefined();
+		expect(obj).toEqual({ newKey: "value" });
 
 		const obj2: Data = { a: 1, b: 2 };
 		const result2 = updateObjectAdvanced(obj2, { $rename: { a: "c" } });
-		expect(result2).toEqual({ c: 1, b: 2 });
-		expect(obj2).toEqual({ a: 1, b: 2 });
+		expect(result2).toBeUndefined();
+		expect(obj2).toEqual({ c: 1, b: 2 });
 	});
 
 	test("13. should handle $set operator", () => {
 		const obj: Data = { a: 1 };
-		let result = updateObjectAdvanced(obj, { $set: { a: 2 } });
-		expect(result.a).toBe(2);
-		expect(obj.a).toBe(1);
+		updateObjectAdvanced(obj, { $set: { a: 2 } });
+		expect(obj.a).toBe(2);
 
-		const previousResult = result;
-		result = updateObjectAdvanced(result, { $set: { b: 3 } });
-		expect(result.b).toBe(3);
-		expect(result.a).toBe(2);
-		expect(previousResult.b).toBe(undefined);
+		const obj2: Data = { a: 1 };
+		updateObjectAdvanced(obj2, { $set: { b: 3 } });
+		expect(obj2.b).toBe(3);
+		expect(obj2.a).toBe(1);
 	});
 
 	test("14. should handle $unset operator", () => {
 		const obj: Data = { a: 1, b: 2, c: 3 };
 		const result = updateObjectAdvanced(obj, { $unset: { b: 1 } });
-		expect(result).toEqual({ a: 1, c: 3 });
-		expect(obj).toEqual({ a: 1, b: 2, c: 3 });
+		expect(result).toBeUndefined();
+		expect(obj).toEqual({ a: 1, c: 3 });
 	});
 
 	test("15. should handle multiple operators in one call", () => {
@@ -173,8 +161,8 @@ describe("updateObjectAdvanced", () => {
 			$merge: { c: { y: 2 } },
 			$unset: { d: 1 },
 		});
-		expect(result).toEqual({ a: 3, b: [1, 2, 3], c: { x: 1, y: 2 } });
-		expect(obj).toEqual({ a: 1, b: [1, 2], c: { x: 1 } });
+		expect(result).toBeUndefined();
+		expect(obj).toEqual({ a: 3, b: [1, 2, 3], c: { x: 1, y: 2 } });
 	});
 
 	test("16. should handle complex nested updates", () => {
@@ -192,29 +180,25 @@ describe("updateObjectAdvanced", () => {
 			},
 		});
 
-		expect(result).toEqual({
+		expect(result).toBeUndefined();
+		expect(obj).toEqual({
 			arr: [1, 2, 3, 4],
 			num: 10,
 			obj: { a: 1, b: 2, c: 3 },
-		});
-		expect(obj).toEqual({
-			arr: [1, 2, 3],
-			num: 5,
-			obj: { a: 1, b: 2 },
 		});
 	});
 
 	test("17. should update fields not starting with $", () => {
 		const obj: Data = { a: 1, b: 2 };
 		const result = updateObjectAdvanced(obj, { c: 3, d: 4 });
-		expect(result).toEqual({ a: 1, b: 2, c: 3, d: 4 });
-		expect(obj).toEqual({ a: 1, b: 2 });
+		expect(result).toBeUndefined();
+		expect(obj).toEqual({ a: 1, b: 2, c: 3, d: 4 });
 	});
 
-	test("18. should handle empty update and return same object reference", () => {
+	test("18. should handle empty update and return undefined", () => {
 		const obj = { a: 1, b: 2 };
 		const result = updateObjectAdvanced(obj, {});
-		expect(result).toBe(obj);
+		expect(result).toBeUndefined();
 		expect(obj).toEqual({ a: 1, b: 2 });
 	});
 });
